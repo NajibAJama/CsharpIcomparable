3074c491f3a76d8688b29e285ad51d1d801f017d
Jama, Najib A <najama@student.rtc.edu>
2019-08-12 22:27:08 -0700

    Createed Person class and implemented  IComparable to provide default order

---
+++ CsharpIcomparable/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpIcomparable
+{
+    public class Person:IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth{ get; set; }
+        public string Nationality { get; set; }
+        //defualt compareTo > compares FirstName 
+        public int CompareTo(object obj)
+        {
+            Person otherPerson = (Person)obj;
+            return this.FirstName.CompareTo(otherPerson);
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName}: {LastName}: {DateOfBirth.ToShortDateString()}: {Nationality}:";
+        }
+    }
+}
